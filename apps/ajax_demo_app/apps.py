3368a3c74c056e731383baf026bda9bff2a052bc
ste ste <steelst0@gmail.com>
2018-07-24 17:46:47 -0700

    initial

---
+++ apps/ajax_demo_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AjaxDemoAppConfig(AppConfig):
+    name = 'ajax_demo_app'
---
+++ apps/ajax_demo_app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # root goes to the index
+    url(r'^$', views.index),
+    url(r'^all.json$', views.all_json),
+    url(r'^all.html$', views.all_html),
+    url(r'^find$', views.find),
+    url(r'^create$', views.create),
+
+]
